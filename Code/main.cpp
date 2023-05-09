1e1db7002450642ee752baf995f962c145f2e227
H-anis <101173357+H-anis@users.noreply.github.com>
2023-05-09 20:09:04 +0200

    ee

---
+++ Code/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#include "Game.h"
+
+
+/*
+	Things todo:
+
+	- Also Figure out a away to PlayAgain in the LostState
+*/
+
+int main()
+{
+	printf("ok");
+	Game snakeGame;
+
+	snakeGame.start();
+
+	return 0;
+}
